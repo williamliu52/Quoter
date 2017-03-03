97a2473f0c68b9341912850a8b53dd79e678706c
WILL LIU <wrliu@uwaterloo.ca>
2017-03-03 15:22:51 -0500

    Set up different environments for staging and production

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
@@ -8,6 +11,6 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}".format(name)
-
+_
 if __name__ == '__main__':
     app.run()
---
+++ config.py
@@ -0,0 +1,27 @@
+mport os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'xman-102312' 
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
